a1542577608142048c59da16abe3aab8163d0f8f
LAPTOP-JQA1MISB\raju <krishnamrajuk23@gmail.com>
2023-04-01 19:15:57 +0530

    chat engine user settings

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ function App() {
   return (
     <ChatEngine
       height="100vh"
-      projectID=""
-      userName=""
-      userSecret=""
+      projectID="3b63dead-e5a3-4e06-bc89-ed1b611a0c4b"
+      userName="krish"
+      userSecret="1234"
     />
   );
 }
