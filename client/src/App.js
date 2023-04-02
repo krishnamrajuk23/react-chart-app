148704735c0a2162bc440c739a93b012ba91c4e0
LAPTOP-JQA1MISB\raju <krishnamrajuk23@gmail.com>
2023-04-02 22:04:55 +0530

    refactoring code

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import { ChatEngine } from 'react-chat-engine'
 import './App.css';
 
 function App() {
+  const userName = localStorage.getItem("userName");
+  const secret = localStorage.getItem("secret");
   return (
     <ChatEngine
       height="100vh"
       projectID="3b63dead-e5a3-4e06-bc89-ed1b611a0c4b"
-      userName="krish"
-      userSecret="1234"
+      userName={userName}
+      userSecret={secret}
     />
   );
 }
